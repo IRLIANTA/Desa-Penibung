5700306a320359231d9be1ece13b5c0873abdf83
IRLIANTA <irliantar@example.comgit config --global --listgit config --global user.name IRLIANTAgit config --global user.email irliantar@example.com>
2025-08-18 18:31:11 +0700

    UI Template

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-    Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me']);
-    Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me'])->name('me');
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
